ae949bf0bc328233f7632a0a63db74b6ea2515e9
DESKTOP-QVLB1UV\PC <bborara93@gmail.com>
2025-11-20 17:40:45 +0900

    비밀번호 암호화(Bcrypt), 토큰 생성(jsonwebtoken)

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
 const bodyParser = require('body-parser');
 
 const config = require('./config/key');
-
 const {User} = require("./models/User");
+
+const cookieParser = require('cookie-parser');
 
 // application/x-www-form-urlencoded 분석해서 가져올 수 있게 해주는 코드
 app.use(bodyParser.urlencoded({extended: true}));
 // application/json
 app.use(bodyParser.json());
-
-
+app.use(cookieParser());
 
 //----------------------------------------------------------------------
 const mongoose = require('mongoose')
@@ -27,13 +28,14 @@ app.get('/', (req, res) => {
   res.send('Hello World! localhost:3000 이건 첫페이지 // nodemon install --start-dev 사용하면 서버 끄고 켜지 않아도 변경내용 확인 가능')
 })
 
-
 //----------------------------------------------------------------------
+// [회원가입]
 // 회원 가입할 때 필요한 정보들을 client에서 가져오면
 // 그것들을 DB에 넣어준다.
-
 // app.post('/register', (req, res) => {
 //   const user = new User(req.body)
+  
+//   // 저장하기 전에 비밀번호 암호화 진행
   
 //   user.save((err, userInfo) => {
 //       if(err) return res.json( { success: false, err })
@@ -43,32 +45,110 @@ app.get('/', (req, res) => {
 //   })
 // })
 
-//==> 위 코드로 postman으로 전송시 응답 에러가 자꾸 나서 아래의 코드로 변경
+//==> 위 코드로 postman으로 전송시 응답 에러가 자꾸 나서 아래의 코드로 변경 
 app.post('/register', async (req, res) => {
   try {
     // 클라이언트에서 보낸 데이터로 User 인스턴스 생성
     const user = new User(req.body)
-    // DB에 저장
-    await user.save()
-    // 성공 응답
-    return res.status(200).json({ success: true })
+    // 콜백 넣지말고, 그냥 await만! (요즘 방식)
+    const userInfo = await user.save()
+    // 저장 성공시!
+    return res.status(200).json({ success : true })
 
   } catch (err) {
-    // MongoDB duplicate email 에러 처리
+    // 이메일 중복 에러 (unique index)
     if (err.code === 11000) {
       return res.status(400).json({
         success: false,
         message: "이미 존재하는 이메일입니다."
       })
     }
-
-    console.error('[/register] error:', err.message)
+    // 에러 메세지 표시
+    console.error('[/register] 에러 메세지 :', err.message)
     return res.status(500).json({
       success: false,
       message: err.message,
     })
   }
 })
+
+//------------------------------------------------------------------------
+// [로그인]
+// 1. 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+// 2. 요청된 이메일이 DB에 있다면 비밀번호가 맞는 비밀번호인지 확인
+// 3. 비밀번호까지 맞다면 토큰 생성
+// app.post('/login', (req, res) => {
+  // 1번
+  // User.findOne({ email : req.body.email }, (err, user) => { 
+  //   // DB에 유저가 없을 경우
+  //   if(!user) {
+  //     return res.json({
+  //       loginSuccess : false,
+  //       message : "제공된 이메일에 해당하는 유저가 없습니다"
+  //     })
+  //   }
+  // DB에 유저가 있을 경우 2번
+  // user.comparePassword(req.body.password, (err, isMatch) => {
+  //   if(!isMatch)
+  //     return res.json({ loginSuccess : false, message : "비밀번호가 틀렸습니다."})
+    
+  //   // 비밀번호가 맞다면 3번
+  //   user.generateToken((err, user) =>  {
+  //     if(err) return res.status(400).send(err);
+      
+  //     // token을 저장 => 어디에? => 쿠키, 로컬스토리지 등..
+  //     // 쿠키에 저장 => cookie-parser 라이브러리 설치 
+  //     res.cookie("x_auth", user.token)
+  //     .status(200)
+  //     .json({ loginSuccess : true, userId : user._id})
+  //   })
+  // })
+  
+// ========> Mongoose 7에서는 findOne(query, callback) 방식이 완전히 금지됨. 아래의 async 코드로 전환.
+app.post('/login', async (req, res) => {
+  try {
+    // 1번
+    const user = await User.findOne({ email : req.body.email });
+    // DB에 유저가 없을 경우
+    if(!user) {
+      return res.json({
+        loginSuccess : false,
+        message : "제공된 이메일에 해당하는 유저가 없습니다"
+      })
+    }
+
+    // DB에 유저가 있을 경우 2번
+    user.comparePassword(req.body.password, async (err, isMatch) => {
+      if(err) return res.status(400).send(err)
+      if(!isMatch) {
+        return res.json({ loginSuccess : false, message : "비밀번호가 틀렸습니다."})
+      }
+      
+      // 비밀번호가 맞다면 3번
+      const updatedUser = await user.generateToken();
+
+      // token을 저장 => 어디에? => 쿠키, 로컬스토리지 등..
+      // 쿠키에 저장 => cookie-parser 라이브러리 설치 
+      return res
+        .cookie("x_auth", updatedUser.token)
+        .status(200)
+        .json({ loginSuccess : true, userId : updatedUser._id})
+      })
+
+  } catch(err) {
+    return res.status(500).send(err);
+  }
+})
+
+
+
+
+
+
+
+
+
+
 
 //------------------------------------------------------------------------
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,10 @@
 // mongoDB Schema 설정
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -13,7 +18,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        maxlength: 50
+        // maxlength: 50 // bcrypt 해시는 60글자라 max 안두는게 안전
     },
     role: {
         type: Number,
@@ -28,6 +33,53 @@ const userSchema = mongoose.Schema({
     }
 
 })
+
+// 저장 전 비밀번호 암호화 진행
+userSchema.pre('save', function( next ) {
+    var user = this; // 위 userSchema를 가리킴
+
+    // 비밀번호가 변경되지 않았으면 그냥 통과
+    if(!user.isModified('password')) return next()
+
+    // 비밀번호 변경된 경우에만 해시 (https://www.npmjs.com/package/bcrypt에 적혀진 구조 확인)
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        if(err) return next(err)
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if(err) return next(err)
+            user.password = hash
+
+            next()
+        })
+    })    
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    //plainPassword:1234 / 암호화된 비밀번호:$2b$10$pNhkB.zMvSdNTwYfzAPbNe8f4mw2yQsKlKiv64Key/KAoIuVEYkwW 
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = async function(cb) {
+    var user = this;
+
+    //jsonwebtoken을 이용해서 token 생성
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    // user._id + 'secretToken' = token 
+    
+    user.token = token
+
+    // user.save(function(err, user) {
+    //     if(err) return cb(err)
+    //     cb(null, user)
+    // })
+    // => 콜백 버전 말고 async 버전으로!
+    await user.save()
+    return user;
+}
+
 
 const User = mongoose.model('User', userSchema)
 module.exports = {User}
